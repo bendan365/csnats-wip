05c6b8cfc3193dd25830d6d50ec41803cd0029eb
Colin Sullivan <colin.sullivan@apcera.com>
2015-11-04 15:11:38 -0700

    Cleanup old directories

--- NATS/simple_build.bat
+++
@@ -1,7 +0,0 @@
-REM A very simple build batch file.
-
-set CSC=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe
-
-%CSC% /out:bin\Release\NATS.Client.DLL /target:library /doc:NATS.XML /optimize+ *.cs Properties\*.cs
-%CSC% /out:bin\Debug\NATS.Client.DLL /target:library /debug+ *.cs Properties\*.cs
-
