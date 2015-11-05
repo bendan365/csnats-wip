f7c8c2d9f490596b1879d227a158d53b6bd06458
Colin Sullivan <colin.sullivan@apcera.com>
2015-11-05 10:37:09 -0700

    Fix Copyright.

--- NATS/Properties/AssemblyInfo.cs
+++ NATS/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Apcera, Inc.")]
 [assembly: AssemblyProduct("NATS")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Apcera 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
